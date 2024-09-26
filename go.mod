16bea5adad60bad029fd3ff3f8b48070c61b4396
chaya2z <taisuke.nakano@chaya2z.com>
2024-09-27 01:48:31 +0900

    Add TOML configuration parsing to main.go
    Introduced a new dotfiles.toml file and implemented TOML configuration parsing in main.go to read and display the version.

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module dotfiles-manager
 
-go 1.21
+go 1.21.0
+
+toolchain go1.21.4
+
+require github.com/pelletier/go-toml/v2 v2.2.3
--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/pelletier/go-toml/v2"
+	"log"
+	"os"
 )
 
+type MyConfig struct {
+	Version string
+}
+
 func main() {
-	fmt.Println("Start...")
+	file, err := os.Open("./testdata/config/dotfiles.toml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := make([]byte, 1024)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var cfg MyConfig
+	if err := toml.Unmarshal(data[:count], &cfg); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(cfg.Version)
 }
